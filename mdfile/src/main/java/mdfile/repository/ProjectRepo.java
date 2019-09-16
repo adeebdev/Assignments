fcde03885ae50a6fcf5528383429d42f0377ee7a
Abdul Hafiz <Abdul.Hafiz@emirates.com>
2019-09-16 17:05:14 +0400

    added again

---
+++ mdfile/src/main/java/mdfile/repository/ProjectRepo.java
@@ -0,0 +1,14 @@
+package mdfile.repository;
+
+import org.springframework.stereotype.Repository;
+
+import mdfile.dto.ProjectDTO;
+
+@Repository("ProjectRepo")
+public class ProjectRepo {
+
+	public ProjectDTO getProjectDetail() {
+		return null;
+
+	}
+}
